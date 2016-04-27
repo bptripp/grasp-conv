ae218bbad882a4dd4d206f52435d32a96501670b
Bryan Tripp <bptripp@uwaterloo.ca>
2016-04-27 11:34:45 -0400

    trying to model success probability

--- py/model.py
+++ py/model.py
@@ -25,7 +25,7 @@ def get_bowls():
     depths = []
     labels = []
     for shape in shapes:
-        f = open('../data/' + shape + '.pkl', 'rb')
+        f = open('../data/' + shape + '-random.pkl', 'rb')
         (d, l) = cPickle.load(f)
         f.close()
         depths.extend(d.tolist())
@@ -34,31 +34,29 @@ def get_bowls():
     return np.array(depths), np.array(labels)
 
 
-imsize = (80,80)
-centres = np.arange(1,1.9,.1)
+imsize = (40,40)
 
 model = Sequential()
-#model.add(AveragePooling2D())
-model.add(Convolution2D(32, 15, 15, input_shape=(len(centres),imsize[0],imsize[1]), init='glorot_normal'))
+model.add(Convolution2D(32, 15, 15, input_shape=(1,imsize[0],imsize[1]), init='glorot_normal', border_mode='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(.5))
-model.add(Convolution2D(32, 5, 5, init='glorot_normal'))
+model.add(Convolution2D(32, 5, 5, init='glorot_normal', border_mode='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(.5))
-#model.add(Convolution2D(32, 5, 5, init='glorot_normal'))
-#model.add(Activation('relu'))
-#model.add(Dropout(.5))
+model.add(Convolution2D(32, 5, 5, init='glorot_normal', border_mode='same'))
+model.add(Activation('relu'))
+model.add(Dropout(.5))
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dropout(.5))
 model.add(Dense(1))
-model.add(Activation('sigmoid'))
+#model.add(Activation('sigmoid'))
 
 adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-model.compile(loss='binary_crossentropy', optimizer=adam)
+model.compile(loss='mean_squared_error', optimizer=adam)
 
 depths, labels = get_bowls()
 
@@ -66,76 +64,21 @@ depths = np.max(depths.flatten()) - depths # more like disparity; background zer
 sigma = np.std(depths.flatten())
 depths = depths / sigma
 
-width = .1
-n_train = 7000
-X_train = np.zeros((n_train, len(centres), imsize[0], imsize[1]))
-Y_train = np.zeros(n_train)
-fail_count = 0
-success_count = 0
-i = 0
-while fail_count < n_train/2 or success_count < n_train/2:
-    if np.mod(i, 1000) < 900:
-        if labels[i] > .5 and success_count < n_train/2:
-            X_train[fail_count+success_count,:,:,:] = disp_tuning(depths[i,:,:], centres, width)
-            Y_train[fail_count+success_count] = labels[i]
-            success_count = success_count + 1
-            #print('adding ' + str(i) + ' to train')
-        if labels[i] <= .5 and fail_count < n_train/2:
-            X_train[fail_count+success_count,:,:,:] = disp_tuning(depths[i,:,:], centres, width)
-            Y_train[fail_count+success_count] = labels[i]
-            fail_count = fail_count + 1
-            #print('adding ' + str(i) + ' to train')
-    i = i + 1
+n_train = 90000
 
+X_train = np.zeros((n_train, 1, imsize[0], imsize[1]))
+X_train[:,0,:,:] = depths[:n_train,:,:]
+Y_train = labels[:n_train]
 
-n_valid = 350
-X_valid = np.zeros((n_valid, len(centres), imsize[0], imsize[1]))
-Y_valid = np.zeros(n_valid)
-valid_count = 0
-for i in range(len(labels)):
-    if valid_count < n_valid and np.mod(i, 1000) >= 900: 
-        #print('adding ' + str(i) + ' to valid')
-        X_valid[valid_count,:,:,:] = disp_tuning(depths[i,:,:], centres, width)
-        Y_valid[valid_count] = labels[i]
-        valid_count = valid_count + 1
+n_valid = 1000
+X_valid = np.zeros((n_valid, 1, imsize[0], imsize[1]))
+X_valid[:,0,:,:] = depths[n_train:n_train+n_valid,:,:]
+Y_valid = labels[n_train:n_train+n_valid]
 
-
-# #balance training data: first 50 successes and first 50 failures
-# n = 500
-# X_train = np.zeros((n,1,28,28))
-# Y_train = np.zeros(n)
-# fail_count = 0
-# success_count = 0
-# i = 0
-# while fail_count < n/2 or success_count < n/2:
-#     if source.Y[i] > .5 and success_count < n/2:
-#         X_train[fail_count+success_count,:,:,:] = source.X[i,:,:,:]
-#         Y_train[fail_count+success_count] = source.Y[i]
-#         success_count = success_count + 1
-#     if source.Y[i] < .5 and fail_count < n/2:
-#         X_train[fail_count+success_count,:,:,:] = source.X[i,:,:,:]
-#         Y_train[fail_count+success_count] = source.Y[i]
-#         fail_count = fail_count + 1
-#     i = i + 1
-#     print(i)
-#
-# print(np.mean(Y_train))
-#
-# # X_train = source.X[:100,:,:,:]
-# # Y_train = source.Y[:100]
-# X_valid = source.X[950:,:,:,:]
-# Y_valid = source.Y[950:]
-
-
-h = model.fit(X_train, Y_train, batch_size=32, nb_epoch=500, show_accuracy=True, validation_data=(X_valid, Y_valid))
+h = model.fit(X_train, Y_train, batch_size=32, nb_epoch=200, show_accuracy=True, validation_data=(X_valid, Y_valid))
 
 Y_predict = model.predict(X_valid)
 print(Y_predict)
-model.to_json('model.json')
 model.save_weights('model_weights.h5')
+model.to_json('model.json')
 
-#
-# import cPickle
-# f = open('model.pkl', 'wb')
-# cPickle.dump(model, f)
-# f.close()
